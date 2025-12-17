7872171b5b463da346fdb5c56e8db4342b411ef7
Mingye Zhu <51037813+stevie1023@users.noreply.github.com>
2025-12-17 15:01:00 +0800

    Update __init__.py

--- openrlhf/trainer/ppo_utils/__init__.py
+++ openrlhf/trainer/ppo_utils/__init__.py
@@ -1,5 +1,5 @@
 from .experience_maker_switch import Experience, NaiveExperienceMaker, RemoteExperienceMaker
-# from .experience_maker_switch import Experience, NaiveExperienceMaker, RemoteExperienceMaker  ###This is for the original GRPO
+# from .experience_maker import Experience, NaiveExperienceMaker, RemoteExperienceMaker  ###This is for the original GRPO
 from .kl_controller import AdaptiveKLController, FixedKLController
 from .replay_buffer import NaiveReplayBuffer
 
